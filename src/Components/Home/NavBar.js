e040e8993fc4d2894d12295c2a83c9f62bdc402f
prince-arun <prince.skillsafari@gmail.com>
2023-10-02 05:22:37 +0530

    Navbar bug fixed

--- src/Components/Home/NavBar.js
+++ src/Components/Home/NavBar.js
@@ -23,22 +23,49 @@ const NavBar = () => {
     name: "",
     email: "",
     password: "",
+    age: "",
+    gender: "",
+    country: "",
+    termsAccepted: false,
     error: null,
     loading: false,
   });
 
   const navigate = useNavigate();
 
-  const { name, email, password, error, loading } = data;
+  const {
+    name,
+    email,
+    password,
+    age,
+    gender,
+    termsAccepted,
+    error,
+    country,
+    loading,
+  } = data;
 
+  // const handleChange = (e) => {
+  //   setData({ ...data, [e.target.name]: e.target.value });
+  // };
   const handleChange = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value });
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setData({ ...data, [name]: newValue });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setData({ ...data, error: null, loading: true });
-    if (!name || !email || !password) {
+    if (
+      !name ||
+      !email ||
+      !password ||
+      !gender ||
+      !age ||
+      !termsAccepted ||
+      !country
+    ) {
       setData({ ...data, error: "All fields are required" });
     }
     try {
@@ -51,6 +78,10 @@ const NavBar = () => {
         uid: result.user.uid,
         name,
         email,
+        age,
+        gender,
+        country,
+        termsAccepted,
         createdAt: Timestamp.fromDate(new Date()),
         isOnline: true,
       });
@@ -58,6 +89,10 @@ const NavBar = () => {
         name: "",
         email: "",
         password: "",
+        age: "",
+        gender: "",
+        country: "",
+        termsAccepted: false,
         error: null,
         loading: false,
       });
@@ -70,7 +105,7 @@ const NavBar = () => {
   //-------------------------------------------------------------------------------------------------------------------------------
 
   return (
-    <div className="navBar position-sticky top-0 left-0">
+    <div className="navBar position-relative z-index-5 top-0 left-0">
       <Navbar className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="#home">
@@ -105,6 +140,41 @@ const NavBar = () => {
                       />
                     </div>
                     <div className="input_container">
+                      <label htmlFor="age">Age</label>
+                      <input
+                        type="number"
+                        name="age"
+                        value={age}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="input_container">
+                      <label>Gender</label>
+                      <div>
+                        <label>
+                          <input
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            checked={gender === "male"}
+                            onChange={handleChange}
+                          />
+                          Male
+                        </label>
+                        <label>
+                          <input
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            checked={gender === "female"}
+                            onChange={handleChange}
+                          />
+                          Female
+                        </label>
+                        {/* You can add more gender options as needed */}
+                      </div>
+                    </div>
+                    <div className="input_container">
                       <label htmlFor="email">Email</label>
                       <input
                         type="text"
@@ -121,6 +191,33 @@ const NavBar = () => {
                         value={password}
                         onChange={handleChange}
                       />
+                    </div>
+                    <div className="input_container">
+                      <label htmlFor="country">Country</label>
+                      <p>
+                        <select
+                          name="country"
+                          value={country}
+                          onChange={handleChange}
+                        >
+                          <option value="USA">USA</option>
+                          <option value="Canada">Canada</option>
+                          <option value="UK">UK</option>
+                          {/* Add more country options as needed */}
+                        </select>
+                      </p>
+                    </div>
+                    <div>
+                      <p>
+                        <input
+                          type="checkbox"
+                          label="Accept the terms & Conditions"
+                          name="termsAccepted"
+                          checked={termsAccepted}
+                          onChange={handleChange}
+                        />
+                        <span>Accept the terms&Conditions</span>
+                      </p>
                     </div>
                     {error ? <p className="error">{error}</p> : null}
                     <div className="btn_container">
